1f5007d191e2c1d75cb5369f7d94417d9a6f2ea8
Renzlawr <RSLogan23@gmail.com>
2022-06-01 15:48:07 -0600

    finished challenges

--- intro-logan-damien.rb
+++ intro-logan-damien.rb
@@ -33,18 +33,32 @@
 
 # ### Strings Challenges
 # - Create a variable and return it in a sentence string interpolation
+my_name = 'Logan'
+# p "Hi #{my_name}"
 # - Create a variable that contains a string and test some of the Ruby string methods
 #   - .upcase
 #   - .reverse
 #   - .include?
 #   - .capitalize
+# p my_name.upcase
+# p my_name.reverse
+# p my_name.include?('o')
+# p my_name.capitalize
 
 # ### Arrays Challenges
 # - Create an array that contains the name of at least five TV shows
+array = ['Mr. Robot', 'Game of Throne', 'The Walking Dead', 'The Simpsons', 'Family Guy']
 # - Find the length of the array
-# - Return the first item in the array
-# - Return the fourth item in the array
-# - Permanently reverse the order of the array
-# - Create a new empty array for your top favorite TV shows
-# - From the full TV show array, add your top two favorite shows to the empty array
+# p array.length
+# # - Return the first item in the array
+# p array.first
+# # - Return the fourth item in the array
+# p array[3]
+# # - Permanently reverse the order of the array
+# p array.reverse
+# # - Create a new empty array for your top favorite TV shows
+# newArr = []
+# # - From the full TV show array, add your top two favorite shows to the empty array
+# newArr << array[0] << array[1]
+# p newArr
 
