ecc4c8d317c5cdf7909b10db1040ccd2ae3c21a4
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-06-01 16:32:53 -0700

    conditional challenge

---
+++ conditional-jb-stephen.rb
@@ -0,0 +1,26 @@
+Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
+
+Story: As a user, I can see a message in the terminal saying if there was a tie.
+
+
+# pseudo code
+# prompt user to either rock, paper or scissors
+# get info and push to a variable
+# give user 2 a prompt to either rock, paper or scissors
+# get info and push to a different variable
+# we assign values between rock paper or scissors
+# we can assign rock >scissors
+# we can assign scissors > paper
+# we can assign paper > rock
+# rock = 20
+# scissors = 10
+# paper = 5
+#line 19 - 20 will not work
+# we can assign lines 16 - 18 into arrays
+
+
+
