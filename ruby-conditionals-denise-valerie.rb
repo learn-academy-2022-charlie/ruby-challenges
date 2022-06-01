ce219373b266482da5b173f9e7fdcbbc71c757a1
Denise-Carlson <carlsonden@gmail.com>
2022-06-01 16:47:47 -0700

    worked on first 3 conditions

---
+++ ruby-conditionals-denise-valerie.rb
@@ -0,0 +1,18 @@
+# Story: As user 1, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+p 'Player1 Please choose rock, paper or scissors?'
+ player1 = gets.chomp
+p 'Player2 Please choose rock, paper or scissors?'
+player2 = gets.chomp
+if player1 == 'paper', 'rock','scissors'
+# roc = 'rock'
+# pap = 'paper'
+# scis ='scissors'
+# p "Do you want to be #{roc},#{paper}, #{scis}?"
+# p "#{roc_pap_scis}"
+
+
+# Story: As user 2, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+# Story: As a user, I can see a message in the terminal saying if user 1 or user 2 won the round.
+
+# Story: As a user, I can see a message in the terminal saying if there was a tie.
