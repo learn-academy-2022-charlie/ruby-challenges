955f98a87153b3fe5947851406ec051ec2470e55
ios10 <trikshotzz05@gmail.com>
2022-06-02 11:39:01 -0700

    account verify

--- methods-jorge-damien-ivan.rb
+++ methods-jorge-damien-ivan.rb
@@ -1,49 +1,49 @@
-## Challenges
+# ## Challenges
 
-# - Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+# # - Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
 
-# Create a method called sum_these_numbers
+# # Create a method called sum_these_numbers
 
-    def sum_these_numbers(num1, num2)
-        num1 + num2
-    end
-    p sum_these_numbers(4, 88)    
+#     def sum_these_numbers(num1, num2)
+#         num1 + num2
+#     end
+#     p sum_these_numbers(4, 88)    
 
-# - Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# # - Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
 
-    def is_even num1
-        if num1.even?
-            true 
-        else
-            false
-        end 
-    end
-    p is_even 7
-    p is_even 8
+#     def is_even num1
+#         if num1.even?
+#             true 
+#         else
+#             false
+#         end 
+#     end
+#     p is_even 7
+#     p is_even 8
 
-# - Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+# # - Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
 
-    def checkNum num
-        if num >= 1 and num <= 10
-            'Valid'
-        else 
-            'Invalid'
-        end
-    end
-    p checkNum 8
-    p checkNum 11
+#     def checkNum num
+#         if num >= 1 and num <= 10
+#             'Valid'
+#         else 
+#             'Invalid'
+#         end
+#     end
+#     p checkNum 8
+#     p checkNum 11
 
-# - Create a method that takes in a string and determines if the string is a palindrome.
+# # - Create a method that takes in a string and determines if the string is a palindrome.
 
-    def palindromeCheck string
-        if string == string.reverse
-            "Yes, #{string} is a Palindrom"
-        else
-            "No, #{string} is not equal to #{string.reverse}"
-        end
-    end
-    p palindromeCheck 'racecar'
-    p palindromeCheck 'toyota'
+#     def palindromeCheck string
+#         if string == string.reverse
+#             "Yes, #{string} is a Palindrom"
+#         else
+#             "No, #{string} is not equal to #{string.reverse}"
+#         end
+#     end
+#     p palindromeCheck 'racecar'
+#     p palindromeCheck 'toyota'
 
 
 # ## Challenge: Password Checker
@@ -59,6 +59,52 @@
 #   - Password _must_ contain at least one of: !#$
 #   - User ID _cannot_ contain the following characters: !#$ or spaces
 #   - Password _cannot_ be the word "password".
+
+    puts "Please enter username"
+    user_name = gets.chomp
+
+    puts "Please enter password"
+    user_password = gets.chomp
+
+        def verify_password (password, username)
+            if password != 'password'
+                if password.length >= 6 && username != password
+                    if password.include?('!') || password.include?('#') || password.include?('$')
+                        # "this is a valid password"
+                        true
+                    else
+                        "password must include !, #, or $"
+                    end
+                else
+                    "password must be greater than six character and not the same as the username"
+                end
+            else
+                "password can not be password"
+            end
+        end
+
+        def verify_username(password, username)
+            if username.length >= 6 && username != password
+                if username.include?('!') || username.include?('#') || username.include?('$') || username.include?(' ')
+                    "username can not include !, #, $, or spaces"
+                else
+                    # "username is valid"
+                    true
+                end
+            else
+                "username must be greater than six character and not the same as the password"
+            end
+
+        end
+        def verify_account(password, username) 
+            if  verify_username(password, username) && verify_password(password, username) 
+                "Account has been created"
+            end
+            
+        end
+
+        p verify_account(user_password, user_name)
+
 
 # ### User Stories: Stretch
 
