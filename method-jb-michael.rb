b9187967e99e79d8c5db9ce195a5dcb5b28ed54d
michael-a-tobias <michael.a.tobias@outlook.com>
2022-06-02 11:58:00 -0700

    Stuck on special character checker for password

--- method-jb-michael.rb
+++ method-jb-michael.rb
@@ -24,28 +24,63 @@
 # p valid_or_not 23
 
 # Create a method that takes in a string and determines if the string is a palindrome.
-def palin string
-  #  we need the reverse?
-  if string == string.reverse
-     "#{string} is a palindrome"
-  else "#{string} is not a palindrome"
-  end
-end
+# def palin string
+#   #  we need the reverse?
+#   if string == string.reverse
+#      "#{string} is a palindrome"
+#   else "#{string} is not a palindrome"
+#   end
+# end
 
-p palin "madam"
-p palin "ears"
+# p palin "madam"
+# p palin "ears"
 
 
 # Challenge: Password Checker
 # User Stories
 # You are writing the user registration page for a secure web site. On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
 
+puts 'Please enter your username.'
+user_name = gets.chomp
+
+puts 'Please enter your password.'
+password = gets.chomp.to_s
+
 # As a developer, I can create a method that checks for the following rules for a user ID and password:
 # User ID and password cannot be the same.
+  # if user_name == password
+  # 'Please choose a password that is not the same as your username.'
+
 # User ID and password must be at least six characters long.
+  # if user_name.length < 6 || password < 6
+  # 'Username and password must be at least 6 characters long.'
+
 # Password must contain at least one of: !#$
+  # password.include? (!, #, $)
+  # "Password must contain at least one of the following: !, #, or $"
+
 # User ID cannot contain the following characters: !#$ or spaces
+  # user_name
+  # "Username cannot contain spaces or the following characters: !, #, or $"
+
 # Password cannot be the word "password".
+  # password == 'password'
+  # 'Your password cannot be "password"'
+
+def log_in(user_name, password)
+  if user_name == password
+    'Please choose a password that is not the same as your username.'
+  elsif password == 'password'
+    'Your password cannot be password.'
+  elsif password.count('!', '#', '$') == 0
+    "Password must contain at least one of the following: !, #, or $"
+  else 'You are logged in.'
+  end
+end
+
+p log_in(user_name, password)
+p password.count('!', '#', '$')
+
 # User Stories: Stretch
 # As a user, I can enter my user ID and password into the terminal after being prompted to find out if the they are acceptable.
 # User Stories: Super Stretch
