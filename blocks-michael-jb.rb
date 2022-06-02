f7b0393c14ede410d2d6443641ff065b804a9104
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-06-02 16:45:37 -0700

    blocks challenge

--- blocks-michael-jb.rb
+++ blocks-michael-jb.rb
@@ -1,7 +1,31 @@
 # Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+
+# lower_case = ['busy', 'cars', 'on', 'the', 'street']
+# def cap_words array
+#     array.map do |value|
+#         value.capitalize()
+#     end
+# end
+# p cap_words lower_case
+
 # Create a method that takes in an array of numbers and returns the largest number in the array.
+# num_array = [4,78,95,9,10]
+# def largest_num array
+#     array.max 
+# end
+# p largest_num num_array
+
 # Create a method that takes in an array of numbers and returns the smallest number in the array.
+
+num_array = [4,78,95,9,10]
+def smallest_num array
+   array.min
+end
+p smallest_num num_array
+
 # Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+
+
 # Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
 # Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
 # FIZZBUZZ
