4ffedd27e237446a92ab2aad964d3b3a7c1ea23d
Davon <davonk12@hotmail.com>
2022-06-03 11:32:31 -0700

    Finished everything but the stetch challenge.

--- hashes-jorge-davon.rb
+++ hashes-jorge-davon.rb
@@ -6,13 +6,13 @@
 
 # As a developer, I can add five key:value pairs of app names and their descriptions to my hash.
 
-    my_phone = {
+my_phone = {
     find_phone: 'Where is my phone',
     settings: 'How my phone works',
     barber_shop: 'Find nearest barber',
-    bbq_spot: 'Locate nearest bbq restaurant',
-    spa_app: 'Locate nearest spa'     
-    }
+    bbq_spot: 'nearest bbq restaurant',
+    spa_app: 'nearest spa'    
+}
         # p my_phone
 
 # As a developer, I can return a value from my_phone by passing in the name of a key.
@@ -36,8 +36,18 @@
     # {:settings=>"How my phone works", :bbq_spot=>"I am hungry", :spa_app=>"I am tired", :locate_phone=>"Where is my phone", :hair_salon=>"Find nearest barber"}
 
 # As a developer, I can delete two key:value pairs from my_phone.
+my_phone.delete(:bbq_spot)
+my_phone.delete(:hair_salon)
+ #p my_phone
 
+#Output:{:settings=>"How my phone works", :spa_app=>"I am tired", :locate_phone=>"Where is my phone"}
 
 # As a developer, I can use an enumerable method to return information about all of my_phone's applications.
+
+
+my_phone.map do |key, value|
+       p "The #{key} app is made to locate #{value}."
+     end
+
 # Stretch Challenges
 # As a developer, I can create a custom method that takes in my_phone and returns an array with the app name capitalized and information about each phone app.
