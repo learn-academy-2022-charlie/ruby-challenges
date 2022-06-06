f4b2dca780c562535ff0c8fa8b1886bf8e877207
jennerdulce <jennerdulce@gmail.com>
2022-06-06 12:51:15 -0700

    met MVP

---
+++ text-game-jenner-davon-don.rb
@@ -0,0 +1,150 @@
+# "_________ __            .___          _____                     .___                    
+# /   _____//  |_ __ __  __| _/__.__.   /  _  l   ____ _____     __| _/____   _____ ___.__.
+# l_____  ll   __l  |  l/ __ <   |  |  /  /_l  l_/ ___ll__  l   / __ |/ __ l /     <   |  |
+# /        l|  | |  |  / /_/ |l___  | /    |    l  l___ / __ l_/ /_/ l  ___/|  Y Y  l___  |
+# /_______  /|__| |____/l____ |/ ____| l____|__  /l___  >____  /l____ |l___  >__|_|  / ____|
+#        l/                 l/l/              l/     l/     l/      l/    l/      l/l/     ".each_char {|c| putc c ; sleep 0.0025; $stdout.flush }
+       
+$total_choices = 0
+
+def make_choice (prompt, choice_one, choice_two, choice_three)
+    puts prompt
+    puts choice_one
+    puts choice_two
+    puts choice_three
+    choice = gets.chomp.to_i
+    if choice == 1
+        $total_choices += 1
+    elsif choice == 2
+        $total_choices += 0
+    elsif choice == 3
+        $total_choices -= 1
+    end
+end
+
+
+def week_one
+    puts "Week 1..."
+    puts "1 ) Talk to instructors."
+    puts "2 ) Do some code challenges."
+    puts "3 ) Study syllabus."
+    choice = gets.chomp.to_i
+    if choice == 1
+        make_choice("How are you enjoying class?","1 ) I love it!", "2 ) It's okay", "3 ) It's pretty difficult.")
+    elsif choice == 2
+        make_choice("How are you enjoying class?","1 ) I love it!", "2 ) It's okay", "3 ) It's pretty difficult.")
+    elsif choice == 3
+        make_choice("Reading the syllabus you feel that..", "1 ) There is a pattern.", "2 ) It is all pretty interesting material.", "3 ) You're don't understand anything.")
+    end
+
+    system("clear")
+
+    puts "After Class.."
+    puts "1 ) Talk to instructors."
+    puts "2 ) Attend class yoga."
+    puts "3 ) Meet up with the cohort study group."
+    choice = gets.chomp.to_i
+    if choice == 1
+        make_choice("How are you enjoying class?","1 ) How did you become an instrucor?", "2 ) It's okay", "3 ) It's pretty difficult.")
+    elsif choice == 2
+        make_choice("How are you enjoying class?","1 ) I love it!", "2 ) It's okay", "3 ) It's pretty difficult.")
+    elsif choice == 3
+        make_choice("What do you do during the study group?", "1 ) You get to know everyone", "2 ) You attentively listen to your cohort speak amongst each other", "3 ) You argue with your classmates")
+    end
+    system("clear")
+end
+
+def week_two
+    puts "Week 2..."
+    puts "1 ) Talk to instructors."
+    puts "2 ) Do some code challenges."
+    puts "3 ) Study syllabus."
+    choice = gets.chomp.to_i
+    if choice == 1
+        make_choice("How are you keeping up with class?","1 ) I feel like there is a deeper meaning", "2 ) I'm still doing okay", "3 ) I have no idea whats going on")
+    elsif choice == 2
+        make_choice("As you are doing the code challenges..","1 ) You feel like coding is meant for you.", "2 ) You feel like you understand some things and have trouble on other parts.", "3 ) You feel overwhelmed.")
+    elsif choice == 3
+        make_choice("Reading the syllabus you feel that..", "1 ) You see hidden messages and understand a deeper meaning.", "2 ) You are informed and understand the material.", "3 ) You are falling asleep while reading.")
+    end
+    system("clear")
+    puts "After Class.."
+    puts "1 ) Talk to instructors."
+    puts "2 ) Attend class yoga."
+    puts "3 ) Meet up with the cohort study group."
+    choice = gets.chomp.to_i
+    if choice == 1
+        make_choice("What do you think about coding for a greater purpose?","1 ) I feel that it is my calling in life.", "2 ) I'll do as much as I can, it pays well and meets my needs.", "3 ) I think that I should take it one step at a time.")
+    elsif choice == 2
+        make_choice("As you are stretching..","1 ) You meditate and you begin hearing the core values in your heard", "2 ) You're getting a good stretch.", "3 ) You feel stressed and walk out.")
+    elsif choice == 3
+        make_choice("What do you do during the study group?", "1 ) You happily collaborate ideas for future projects", "2 ) You follow instructions and go with the flow", "3 ) You disagree with future projects")
+    end
+    system("clear")
+end
+
+def week_three
+    puts "Week 3..."
+    puts "1 ) Talk to instructors."
+    puts "2 ) Do some code challenges."
+    puts "3 ) Study syllabus."
+    choice = gets.chomp.to_i
+    if choice == 1
+        make_choice("Would you be interested in meeting some of our staff?","1 ) Sure, I'd love to!", "2 ) I could try to make some time", "3 ) Sorry I have other things to do")
+    elsif choice == 2
+        make_choice("As you do code challenges..","1 ) These seem to be instructions", "2 ) I got my web application working", "3 ) You think that coding is not fun at all")
+    elsif choice == 3
+        make_choice("As you go through the syllabus..", "1 ) You notice that there are hints for the true purpose of STUDY academy.", "2 ) You think that you are digesting the material well.", "3 ) You get distracted and close the browser")
+    end
+
+    system("clear")
+
+    puts "After Class.."
+    puts "1 ) Talk to instructors."
+    puts "2 ) Meet up with the cohort study group."
+    choice = gets.chomp.to_i
+    if choice == 1
+        make_choice("Hey we really like how fast you're progressing, would you like to do more?","1 ) Yeah I am ready for more", "2 ) I'll think about it", "3 ) No thanks.")
+    elsif choice == 2
+        make_choice("You get together with your study group", "1 ) You and your classmates discuss hidden messages from the syllabus", "2 ) You solve and review questions", "3 ) Some of your classmates talk about stranges and weird things happening. They feel like someone is watching them all the time")
+    end
+    system("clear")
+end
+
+def welcome_onboard
+    puts "Hello! Welome to Study Academy!".each_char {|c| putc c ; sleep 0.01; $stdout.flush }
+    puts "What is your name?".each_char {|c| putc c ; sleep 0.01; $stdout.flush }
+    player = gets.chomp
+    puts "Nice to meet you #{player}! We are your instructors Chaustin and Arlene.".each_char {|c| putc c ; sleep 0.01; $stdout.flush }
+    make_choice("Tell us a little about yourself!", "1 ) I am very social.", "2 ) Sometimes I like to socialize and sometimes I don't.", "3 ) I am introverted.")
+    puts "We value your time here at STUDY."
+    puts "Our core values are: Growth, Loyalty, and Teamwork."
+    puts "We are looking forward to having you join us."
+    system("clear")
+end
+
+def check_total_choices_week_one
+    if $total_choices >= 0
+        puts "You continue with STUDY academy"
+    elsif $total_choices < 0
+        puts "You choose to leave."
+    end
+end
+
+def check_total_choices_week_two
+    if $total_choices == 0
+        puts "You get your certificate and leave."
+    elsif $total_choices > 0
+        puts "You join the organization"
+    elsif $total_choices < 0
+        puts "You know too much and your memory gets wiped."
+    end
+end
+
+welcome_onboard
+week_one
+week_two
+check_total_choices_week_one
+puts "You see that some classmates have left the class.."
+week_three
+check_total_choices_week_two
