d7bea32d7b1ed855ab402c79d0ff8b043f5324da
Nic Last <niclast7611@gmail.com>
2022-06-06 15:37:31 -0700

    basic structure down needs refactoring

--- chanelisland-nic-denise-frank.rb
+++ chanelisland-nic-denise-frank.rb
@@ -1,4 +1,22 @@
 # make a parent with the game name 
+
+
+
+
+
+# def game_over 
+#     p "
+#     ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   
+#     █       █      █  █▄█  █       █  █       █  █ █  █       █   ▄  █  
+#     █   ▄▄▄▄█  ▄   █       █    ▄▄▄█  █   ▄   █  █▄█  █    ▄▄▄█  █ █ █  
+#     █  █  ▄▄█ █▄█  █       █   █▄▄▄   █  █ █  █       █   █▄▄▄█   █▄▄█▄ 
+#     █  █ █  █      █       █    ▄▄▄█  █  █▄█  █       █    ▄▄▄█    ▄▄  █
+#     █  █▄▄█ █  ▄   █ ██▄██ █   █▄▄▄   █       ██     ██   █▄▄▄█   █  █ █
+#     █▄▄▄▄▄▄▄█▄█ █▄▄█▄█   █▄█▄▄▄▄▄▄▄█  █▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█
+#      "
+# end
+
+
 def chanelisland 
 
     p "What is your name brave adventurer?" 
@@ -9,7 +27,63 @@ def chanelisland
     p "#{username}, type 1 to walk around, type 2 to do nothing. Choose wisely" 
         descision1 = gets.chomp   
             if descision1 == "1" 
-                p "you got it dude" 
+            
+                p 'What direction would you like to go? North, South, East, West ' 
+                directions = gets.chomp 
+                    if directions == 'North'
+                        p 'Its a cave, looks very dark and scary, but theres no turning back now!!!!'
+                        p 'You have the option to go left or right in the cave'
+                        cavedir = gets.chomp
+                        if cavedir == 'left'
+                            p 'WOW to the left was a treasure chest full of gold, jewels, an old map.'
+                            p 'Would you like to follow the ancient map of the island or stay in the cave and guard your new found treasure? Choose 1 to follow the map or 2 stay with the gold'
+                            caveleftdir = gets.chomp
+                                if caveleftdir == '1'
+                                    p 'Looks like the map led to an old dingie on the otherside of the island that Captain Jack Sparrow used way back when you escaped!!!!'
+                                else 
+                                    p 'You are greedy and rotted in cave forever guarding your treasure.'
+                                    p 'GAME OVER'
+                                     
+                                end
+                            elsif cavedir == 'right'
+                            p ' Do you hear something?!?!!?'
+                            p 'UH OH you have ran into couped up dragon that has been waiting for an unlucky adventurer to come stubble upon him for a meal. '
+                            p "OM NOM NOM NOM Yummy - says the Dragon "
+                            p 'GAME OVER'
+                        end
+                    elsif directions == 'South'
+                        p 'You have stumbled upon a beautiful beach stretching for hundreds of yards. As you walk along this beach you stumble upon someone else who survived the crash...'
+                        p 'WWWWWIIIILLLLSSSSOOOONNNNNN!!!!'
+                        p 'Congratulations you have found a hidden guide of the island. Just like he Tom Hanks in Castaway Wilson will keep you company on this unforgiving island until you get help.'
+
+                    elsif directions == 'East' 
+                        p 'You have stumbled upon the first half of the downed plane. Do you wish to explore the wreck or explore more of the island. Choose 1 to explore the wreck or 2 to turn back.'
+                        planedir = gets.chomp
+                        if planedir == '1'
+                            p 'Good choice adventurer you found tools to survive the island. A cargo ship was passing by and you used the flare gun you found to signal him down! You survived!!!'
+                        elsif planedir == '1'
+                            p 'You missed the ship passing by and you were eaten alive by coconut crabs!'
+                            p 'GAME OVER'
+                        end
+                    elsif directions == 'West'
+                            p 'You have entered a dense forest filled with formidable creatures.'
+                            p 'You have the option to go left or right. Choose 1 for left or 2 for right'
+                            forestdir = gets.chomp
+                            if forestdir == '1'
+                                p 'To the left you found an ax on the floor. You use this ax to cut down the foliage in your way, you see a pirate ship in the distance. If you want to take your chances with the infamous Captain Jack Sparrow and his mighty crew or take you chances with the forest. Choose 1 for Jack Sparrow or 2 for the forest.'
+                                forestdir2 = gets.chomp
+                                if forestdir2 == '1'
+                                    p 'Captain Jack Sparrow considers you a worthy adventurer and allows you passage onto his ship.'
+                                else 
+                                    p 'In the middle of the night you were attacked by a jaguar and didnt make it through the night.'
+                                    p 'GAME OVER'
+                                end
+                            elsif forestdir == '2'
+                                p 'To the right was a tribe a wild monkeys that took you hostage.'
+                                p 'GAME OVER'
+                            end
+                    end
+
             elsif descision1 == "2"
                 p "we warned you brave adventurer, no cowards allowed on this island!"
             else
@@ -17,7 +91,10 @@ def chanelisland
 
 # else need to loop back to 1 or 2
 # need to add end of game 
-            end 
+            end
+            
+            
+
 # Or // Sit and cry 
 # Walk around 
 # What direction would you like to go? North, South, East, West  
