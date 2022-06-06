afd02c2c06c5e5994c36fcc5073e791cb2129bed
CJOcode <cjoconnor24@gmail.com>
2022-06-06 10:31:23 -0700

    started pseudo

---
+++ game-collin-sean-jorge.rb
@@ -0,0 +1,17 @@
+# pseudo code
+# Story Line - survival senerio, jungle, make it to the nearest town; which is filled with zombies, have a knife and flashlight.
+
+# Starting story- with you on a dive exbidition on a cruise, when you come up to the surface the boat is gone, and you swim to a near by beach at night.
+
+#first choice - make a trek to a light in the distance through a thick jungle or stay the night on the beach.
+    # choices trek at night or spend the night on beach - if trek encounter a three headed jaguar (trigar)
+        # choices run or fight - if run you die, if fight you survive
+    # spend night on beach - wake up in the morning and make your way towards the light you saw last night. During trek in the day cross pathes with king Murder hornet. 
+        # choices fight or communicate 
+    
+# second checkpoint 
+
+p 'Final Expedition'
+p ''
+
+    
