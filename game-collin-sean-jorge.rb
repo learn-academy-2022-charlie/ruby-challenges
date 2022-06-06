77221ae4b1ebe3a29e46d2eff22c17f4c741a088
LoneWanderer101 <sgtlinares@gmail.com>
2022-06-06 14:19:55 -0700

    started text-based game challenge

--- game-collin-sean-jorge.rb
+++ game-collin-sean-jorge.rb
@@ -12,6 +12,42 @@
 # second checkpoint 
 
 p 'Final Expedition'
-p ''
+p 'You went on a night diving expedition. As you emerge from the sea, the boat is GONE!!! You decide to swim towards the shore'
+p 'Player enter your name'
+    user_name = gets.chomp
+
+p "#{user_name}, you are on the beach in pitch black under a new moon. All you have on you is a fishing knife and a flashlight. In the far off distance you notice a faint light. What do you do next? Will you decide to head into the jungle towards the light or do you wait it out until the morning on the beach?"
+p "#{user_name}, type trek or beach" 
+    user_name = gets.chomp
+
+if user_name == 'trek'
+    p 'Into the jungle you encounter the three headed jaguar known as a Triguar. Do you decide to run or fight? Enter: run or fight'
+        user_name = gets.chomp
+            if user_name == 'run'
+                p 'You make the cowardly choice. Now you know that you never turn your back on the powerful Triguar. Your journey is OVA'  # Need to end code here. As of now the ending is displayed after this choice even though the journey is over.
+            else 
+                p 'You reach for you knife and take a stab at the Triguar as he pounces on you. As the Triguar lands on you, you manage to stab him in the heart but you get knocked unconsious. You wake up in the morning drenched in the Triguars blood. You continue on your journey towards the light'
+            end
+else
+    p 'You build a fire and survive the night. In the morning you make your way in the direction of the light'         
+end
+
+p 'As you proceed on your journey you reach a river crossing. Do you decide to cross the bridge or find an alternate route. Enter: bridge or alternate'
+    user_name = gets.chomp
+        if user_name == 'bridge'
+            p 'The bridge is military grade and breaks in half. You are now in the river and get eaten by dinosaur sized piranahs. Your journey if OVA'
+        elsif
+            p 'You walk along the river bank and you find a thick vine hanging from a gargantuan tree. On the bank you see a raft made from human bones. What do you do? Will you bust a Tarzan or ride the raft of Charon? Enter: tarzan or charon'
+        else
+                p '#{user_name}, your only choices are bridge or alternate'
+                # edit to allow specific text  
+    end
+        user_name = gets.chomp
+
+
+p 'You arrive at a village. In the distance you see a group of people. As you approach them, they turn around and you see zombies... Then your alarm goes off and it is time to live the dream and check in with Austin and Charlean at Learn Academy'
+
+
+
 
     
